c486eae26deaf8917739e5c39148c85637d09b28
RizkySyahry <85144164+RizkySyahry@users.noreply.github.com>
2021-06-05 21:01:14 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ var r = express.Router();
 
 // load pre-trained model
 const model = require('./sdk/model.js'); // predict
-const cls_model = require('./sdk/mcls_odel.js'); // cls
+const cls_model = require('./sdk/cls_model.js'); // cls
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
@@ -11,12 +11,12 @@ const token = '1749316513:AAFFod2CNPR0eE2ECvIH4cly-tRqyowJvu0'
 const bot = new TelegramBot(token, {polling: true});
 
 state = 0;
-//Main Menu Bot
+// Main Menu Bot
 bot.onText(/\/start/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /predict `
+        click /predict`
     );
 	state = 0;
 });
@@ -32,38 +32,40 @@ bot.onText(/\/predict/, (msg) => {
 
 bot.on('message', (msg) => {
     if(state == 1){
-        s =msg.text.split("|");
-        i = parseFloat(s[0])
-        r = parseFloat(s[1])
-	    
+        s = msg.text.split("|");
 	model.predict(
-	   [
-             i, // string to float
-             r
-	   ]
+	[
+	parseFloat(s[0]), // string to float
+        parseFloat(s[1])
+	]
 	).then((jres1)=>{
 		console.log(jres1);
 				
-		cls_model.classify([i, r, parseFloat(jres1[0]), parseFloat(jres1[1])]).then((jres2)=>{p]).then((jres2)=>{
+		cls_model.classify([parseFloat(s[0]), parseFloat(s[1]), parseFloat(jres1[0]), parseFloat(jres1[1])]).then((jres2)=>{
 		bot.sendMessage(
 	       		msg.chat.id,
-			`nilai v yang diprediksi adalah ${v} volt`
+			`nilai v yang diprediksi adalah ${jres1[0]} volt`
 	  	  );  
           	  bot.sendMessage(
 			msg.chat.id,
-			`nilai P yang diprediksi adalah ${p} watt`
+			`nilai P yang diprediksi adalah ${jres1[1]} watt`
 	  	  );
 		bot.sendMessage(
 	       		msg.chat.id,
 			`Klasifikasi Tegangan ${jres2}`
 		})
+		state = 0
     	  })     
 	})
     }else{
-        state = 0
+	    bot.sendMessage(
+		msg.chat.id,
+		`Please Click / start`
+		);
+	    state = 0;
     }
-});
-
+})
+	    
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
     model.predict(
@@ -95,6 +97,6 @@ r.get('/classify/:i/:r', function(req, res, next) {
 		res.json(jres_)
 		})
     	  })     
-	})
+	});
 			
 module.exports = r;
