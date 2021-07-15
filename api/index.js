3bfd34c88dd4a68e85fb7377970afa09ea4ecbb0
RizkySyahry <85144164+RizkySyahry@users.noreply.github.com>
2021-07-15 18:55:17 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1630793924:AAGJ7WmAw1_qC_sVqv_ZLIxFNHFI6UtjcBs'
+const token = '1749316513:AAFFod2CNPR0eE2ECvIH4cly-tRqyowJvu0'
 const bot = new TelegramBot(token, {polling: true});
 
 
